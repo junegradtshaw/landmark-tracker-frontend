5a77e9ca0886cd2a8e902ab88a8722e3d7aa2514
June Shaw <june.shaw@me.com>
2016-03-07 07:17:03 -0700

    rearrange for gh-pages deploy

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var app = angular.module("myApp", ['ngRoute', 'ngResource']);
+
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'MainController'
+      })
+      .when('/landmarks', {
+        templateUrl: 'partials/landmarks/index.html',
+        controller: 'LandmarksIndexController'
+      })
+      .when('/cats/new', {
+        templateUrl: 'partials/cats/new.html',
+        controller: 'CatsNewController'
+      })
+      .when('/cats/:id', {
+        templateUrl: 'partials/cats/show.html',
+        controller: 'CatsShowController'
+      })
+    $locationProvider.html5Mode(true);
+});

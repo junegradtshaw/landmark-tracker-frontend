0acd9fa618a3873c8f1b0eeb1d8d432264c9c0ac
June Shaw <june.shaw@me.com>
2016-03-08 14:10:33 -0700

    recent fixes

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ var app = angular.module("myApp", ['ngRoute', 'ngResource']);
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: '/landmark-tracker-frontend/partials/home.html',
+        templateUrl: 'landmark-tracker-frontend/partials/home.html',
         controller: 'MainController'
       })
       .when('/landmarks', {
         // templateUrl: 'landmark-tracker/partials/landmarks/index.html',
-        templateUrl: '/landmark-tracker-frontend/partials/landmarks/index.html',
+        templateUrl: 'landmark-tracker-frontend/partials/landmarks/index.html',
         controller: 'LandmarksIndexController'
       })
       .when('/cats/new', {
